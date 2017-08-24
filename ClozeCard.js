f6d76810d898dc711baa34be6f4dd12c9d58e386
joe-fuentes <joevfuentes@aol.com>
2017-08-24 01:09:24 -0400

    Updated files

---
+++ ClozeCard.js
@@ -0,0 +1,18 @@
+// Constructor function for the 'Cloze Card'.
+function ClozeCard(fullText, cloze, partial) {
+    this.fullText = fullText;
+    this.cloze = cloze;
+    this.partial = fullText.split(cloze);
+};
+
+// Constructor that creates a prototype of ClozeCard to return the question missing cloze
+function ClozeCardPrototype() {
+	console.log("Inside!");
+    this.clozeDeleted = function () {
+        return `${this.partial[0]} ____ ${this.partial[1]}`;  //Template literal enclosed by the back-tick ` allows embedded expressions wrapped with ${}
+    };											
+};
+
+ClozeCard.prototype = new ClozeCardPrototype();
+
+module.exports = ClozeCard; 
